91e7acca9473a77bc8e330b71272cdb9b6868c32
salcampagna <sal_campagna@yahoo.com>
2018-05-16 01:49:29 +0000

    another test

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
 #This is a test file
+print("This is the test.py file")
+#I think codenvy only runs the main.py file, 
+#more than likeley main.py would call test.py
+
+name = ("Sal")
+print("my name is " name)
